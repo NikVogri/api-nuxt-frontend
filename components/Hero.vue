861023e2e70e708e46376e034c2f15b63fcf7a41
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-12 23:55:42 +0100

    feat: hero

---
+++ components/Hero.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hero">
+    <transition name="heroText" appear>
+      <div>
+        <HeroNotification :updatedAt="new Date(updatedAt)" />
+        <div class="hero__text">
+          <h1>Corona Virus Statistics and Visualization</h1>
+          <p>
+            Daily updated data from over 180 countries. View data or use our API
+            to display data in your own applications
+          </p>
+        </div>
+        <div class="hero__buttons">
+          <nuxt-link to="/visualization" class="btn btn-primary mr-8"
+            >Visualization</nuxt-link
+          >
+          <nuxt-link to="/api" class="btn btn-dark">Public API</nuxt-link>
+        </div>
+        <HeroQuote />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    updatedAt: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  max-width: 740px;
+  @apply flex mx-auto;
+
+  &__text {
+    h1 {
+      font-size: 70px;
+      line-height: 1;
+      @apply mb-4 font-bold text-center;
+      color: var(--primary-blue);
+    }
+    p {
+      @apply text-gray-800 mb-16 text-lg text-center;
+    }
+  }
+
+  .hero__buttons {
+    @apply flex justify-center mb-16;
+  }
+}
+
+.heroText-enter-active {
+  animation: dropIn 0.8s ease;
+}
+
+@keyframes dropIn {
+  from {
+    opacity: 0;
+    transform: translateY(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+</style>
---
+++ components/HeroNotification.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="notification">
+    <div class="notification__type">Update!</div>
+    <div class="notification__text">Latest API update: {{ updatedAt }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    updatedAt: {
+      type: Date,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.notification {
+  @apply w-full flex border border-gray-400 rounded-lg mt-16 mb-24;
+
+  &__type {
+    background-color: var(--primary-blue);
+    color: var(--white);
+
+    @apply rounded-lg text-center py-2 font-bold;
+    width: 145px;
+  }
+  &__text {
+    @apply text-center py-2 flex-1 font-medium;
+  }
+}
+</style>
