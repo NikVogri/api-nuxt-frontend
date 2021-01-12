4205de080c4fd5da0b7d5e034e0c9bdb9c3ac698
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-12 23:55:09 +0100

    feat: quotes

---
+++ assets/quotes.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    quote: "Don't worry, be happy",
+    author: "Bobby McFerrin"
+  },
+  {
+    quote:
+      "I don’t think of all the misery, but of the beauty that still remains.",
+    author: "Anne Frank"
+  },
+  {
+    quote:
+      "Hope is a renewable option: If you run out of it at the end of the day, you get to start over in the morning",
+    author: "Elizabeth Gilbert"
+  },
+  {
+    quote:
+      "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+    author: "Helen Keller"
+  },
+  {
+    quote: "Hope is the one thing that sometimes keeps us going.",
+    author: "Catherine Pulsifer"
+  }
+];
