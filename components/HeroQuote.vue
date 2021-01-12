3d9bf4399b33fac16412672ad7f5ba303e7f3706
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-12 23:56:47 +0100

    feat: random quote component

---
+++ components/HeroQuote.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="quote">
+    <p>"{{ quote }}"</p>
+    <span>{{ author }}</span>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import quotes from "~/assets/quotes";
+export default Vue.extend({
+  data() {
+    return {
+      quote: "",
+      author: ""
+    };
+  },
+  created() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    this.quote = randomQuote.quote;
+    this.author = randomQuote.author;
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.quote {
+  width: 100%;
+  @apply text-center;
+
+  p {
+    @apply mb-2;
+  }
+  span {
+    @apply font-medium;
+  }
+}
+</style>
