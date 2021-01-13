b5928b4705af81bcfc53ac37ebc1fa3d2b22557c
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-13 22:21:13 +0100

    fix: plugins filter error

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: "~/plugins/filters.ts", mode: "client" }],
+  plugins: ["~/plugins/filters.ts"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
