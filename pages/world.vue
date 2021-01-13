bb150ede76a876c068b9fe1c2ca0c105ba3fd743
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-13 22:22:13 +0100

    feat: world page

---
+++ pages/world.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="world">
+    <World :data="worldData" :animateOnScroll="false" />
+  </section>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    const res = await this.$axios.$get("/world");
+    const worldData = res.data;
+
+    this.worldData = worldData[worldData.length - 1];
+  },
+  data() {
+    return {
+      worldData: null
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.world {
+  min-height: var(--full-height);
+  @apply flex justify-center items-center;
+}
+</style>
