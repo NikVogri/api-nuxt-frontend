9daf4b44158b138e789dc40c8a3cef52d3f155bc
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-14 23:08:36 +0100

    feat updated formatDate filter

--- plugins/filters.ts
+++ plugins/filters.ts
@@ -2,16 +2,21 @@ import Vue from "vue";
 
 import months from "~/assets/months";
 
-Vue.filter("formatNumber", (num: string) => {
+Vue.filter("formatNumber", (num: string): string => {
   return num.toLocaleString();
 });
 
-Vue.filter("formatDate", (date: Date) => {
-  const day = date.getDay();
+Vue.filter("formatDate", (date: Date): string => {
+  const day = date.getDate();
   const monthIndex = date.getMonth();
   const year = date.getFullYear();
-
   const time = date.toTimeString().slice(0, 8);
 
-  return `${months[monthIndex]} ${day}. ${year} at ${time}`;
+  const todaysDate = new Date();
+
+  if (todaysDate.getDate() == day && todaysDate.getMonth() == monthIndex) {
+    return `Today at ${time}`;
+  }
+
+  return `${months[monthIndex]} ${day}, ${year} at ${time}`;
 });
